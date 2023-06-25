8893cdf2ab9295060a89945404a5eba855c82b52
Светлана Югай <135803951+SvetlanaYgay@users.noreply.github.com>
2023-06-25 16:41:14 +0600

    добавали2

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿//min & max
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
