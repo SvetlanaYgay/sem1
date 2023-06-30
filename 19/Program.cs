9b57fef58c981c47e083acbfb8f8cd437155816a
Светлана Югай <135803951+SvetlanaYgay@users.noreply.github.com>
2023-06-30 16:54:53 +0600

    Add files via upload

---
+++ 19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("ВВедите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool IsPalindrome(int number)
+{
+    if (number / 10000 == number % 10 &&
+          number / 1000 % 10 == number % 100 / 10)
+          return true;
+          return false;
+}
+
+Console.WriteLine(IsPalindrome(num));
---
+++ 21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter X for first point ");
+int aX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y for first point ");
+int aY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Z for first point ");
+int aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter X for second point ");
+int bX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y for second point ");
+int bY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Z for second point ");
+int bZ = Convert.ToInt32(Console.ReadLine());
+
+double a = Math.Pow((aX-bX),2);
+double b = Math.Pow((aY-bY),2);
+double c = Math.Pow((aZ-bZ),2);
+
+double length = Math.Sqrt(a+b+c);
+Console.WriteLine($"Расстояние между двух точек в трехмерном пространстве --> {Math.Round (length, 2)}");
+
+
